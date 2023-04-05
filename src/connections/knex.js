1c8b44b235ba0b84767185b88ebc5917a3a07849
Jhohan <jhohan.patrick1@gmail.com>
2023-04-05 09:51:36 -0300

    conectando com banco de daods

---
+++ src/connections/knex.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORTA,
+    }
+})
+
+module.exports = knex
