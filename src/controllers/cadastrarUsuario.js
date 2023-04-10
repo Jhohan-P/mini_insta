67746f5fc4d8520eb69804047ea94ba123fe2599
Jhohan <jhohan.patrick1@gmail.com>
2023-04-10 18:27:38 -0300

    emplementado validacoes para cadastrar usuario

--- src/controllers/cadastrarUsuario.js
+++ src/controllers/cadastrarUsuario.js
@@ -4,7 +4,24 @@ const bcrypt = require('bcrypt')
 const cadastrarUsuario = async (req, res) => {
     const { username, senha } = req.body
 
+    if (!username) {
+        return res.status(400).json({ message: 'O campo username e obrigatório' });
+    }
+    if (!senha) {
+        return res.status(400).json({ message: 'O campo senha e obrigatório' });
+    }
+
+    if (senha.length < 6) {
+        return res.status(400).json({ message: 'A senha deve ter no mínimo 6 caracteres' });
+    }
+
     try {
+        const usuarioExiste = await knex('usuarios').where({ username }).first();
+
+        if (usuarioExiste) {
+            return res.status(401).json({ message: 'Usuário indisponível' });
+        }
+
         const senhaCriptografada = await bcrypt.hash(senha, 10);
 
         const novoConta = await knex('usuarios')
